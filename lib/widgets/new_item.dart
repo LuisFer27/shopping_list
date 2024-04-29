c191b2365ab18e1b5bf7118e4a2194386c862e31
luis <luisfernando.anemona@gmail.com>
2024-04-29 16:09:51 -0600

    cambios

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+//import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list/models/grocery_item.dart'; //paquete agrupado
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -16,16 +20,51 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
-  void _saveItem() {
+  var _isSending = false;
+
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory),
+      setState(() {
+        _isSending = true;
+      });
+      final url = Uri.https('flutter-prep-13c36-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      final response = await http.post(
+        //.patch(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
       );
+      //print(response.body);
+      //print(response.statusCode);
+      final Map<String, dynamic> resData = json.decode(response.body);
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop(GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
+      //.then((response){
+      //response.statusCode == 200;  //200-201 funciona,400-404,500 no funciona
+      //});
+      //Navigator.of(context).pop(
+      //GroceryItem(
+      //    id: DateTime.now().toString(),
+      //    name: _enteredName,
+      //    quantity: _enteredQuantity,
+      //    category: _selectedCategory),
+      //);
       //print(_enteredName);
       //print(_enteredQuantity);
       //print(_selectedCategory);
@@ -126,14 +165,22 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,
-                    child: const Text('Add Item'),
+                    onPressed: _isSending ? null : _saveItem,
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : const Text('Add Item'),
                   )
                 ],
               )
